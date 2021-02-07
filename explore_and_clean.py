d7b52be466a8629e936981ebe8ea82262aba27ff
jonasalx <jonathan.ulloa.icinf@gmail.com>
2021-02-07 01:00:03 -0300

    completando exploracion col fecha y correccion registration_year

--- explore_and_clean.py
+++ explore_and_clean.py
@@ -2,6 +2,7 @@ from typing import List
 import numpy as np
 import pandas as pd
 import re
+import matplotlib.pyplot as plt
 
 #PARTE 1
 autos = pd.read_csv("autos.csv/autos.csv")
@@ -75,6 +76,10 @@ for o in list:
 
 
 new_columns["kilometer"] = "odometer_km"
+new_columns["year_of_registration"] = "registration_year"
+new_columns["month_of_registration"] = "registration_month"
+new_columns["not_repaired_damage"] = "unrepaired_damage"
+new_columns["date_created"] =  "ad_created"
 autos.rename( columns = new_columns, inplace=True )
 
 autos.odometer_km.dtypes #int64
@@ -129,4 +134,92 @@ autos.price.describe()
 
 #PARTE 5
 
+################
+# date_crawled #
+################
 
+#autos.date_crawled.value_counts().sort_index()
+#autos.date_crawled.value_counts(normalize = True, dropna = False).sort_index()
+autos.date_crawled.value_counts(normalize = True, dropna = False).sort_index().describe()
+#los datos se ven bien distribuidos
+
+#por minutos
+autos.date_crawled.str[: 16].value_counts()
+
+#por hora
+autos.date_crawled.str[: 13].value_counts()
+autos.date_crawled.str[: 13].value_counts().sort_values(ascending=False).head(20)
+# 2016-03-05 14    2305
+# Sale de lo normal, ese dia y esa hora 
+
+# no identifico algo anormal en la distribucion de esa hora 
+autos_hour_14 = autos[autos["date_crawled"].between("2016-03-05 14:00:00", "2016-03-05 15:00:00") ]
+autos_hour_14.date_crawled.describe()
+autos_hour_14.date_crawled.value_counts().sort_index().describe()
+
+#que tiene de especial esta fecha, por que tiene tantos "rastreos"
+
+
+
+
+################
+# date_crawled #
+################
+autos.date_created.str[: 10].value_counts().sort_index()
+autos.date_created.str[: 10].value_counts().sort_values(ascending=False).head(50)
+autos.date_created.str[: 10].value_counts().sort_index(ascending=False).head(50)
+#se marca un incremendo desde 2016-03-03      415
+
+#podemos graficar la dispersion
+c = autos.date_created.str[: 10].value_counts().sort_index()
+df = pd.DataFrame([[key, c[key]] for key in c.keys()], columns=['date', 'count'])
+disp = df.plot( "date","count",  kind="scatter" )
+plt.show()
+
+
+
+#autos.last_seen.value_counts().sort_index()
+#autos.last_seen.value_counts(normalize = True, dropna = False).sort_index()
+autos.last_seen.value_counts(normalize = True, dropna = False).sort_index().describe()
+
+#TERMINAR
+
+
+#registration_year
+autos[autos["registration_year"] < 1900 ] = np.nan
+autos[autos["registration_year"] > 2022 ] = np.nan
+
+autos.registration_year.describe()
+"""
+count    371346.000000              bajo la cantidad debido a no considerar nan (<1900 & >2022)
+mean       2003.348489              min y max coinciden por las reglas de nan aplicadas
+std           7.776980
+min        1910.000000
+25%        1999.000000              el 25% se encuentra hasta 1999
+50%        2003.000000              el 50% se encuentra hasta 2003
+75%        2008.000000
+max        2019.000000
+"""
+
+#para ver mejor los minimos, maximos y por que el promedio
+#histograma
+plt.hist(autos.registration_year)
+
+
+
+#PARTE 6
+
+autos[autos["registration_year"] > 2016 ] = np.nan
+"""
+count    356780.000000
+mean       2002.779805
+std           7.395775
+min        1910.000000
+25%        1999.000000
+50%        2003.000000
+75%        2008.000000
+max        2016.000000
+"""
+
+#es seguro dejar como nan
+#ver si se pueden eliminar por completo
